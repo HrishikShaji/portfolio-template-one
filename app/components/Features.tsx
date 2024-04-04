f5bc287a55d7902bdf83aecd513291d62b05f7d1
HrishikShaji <hrishikminishaji@gmail.com>
2024-04-04 16:31:57 +0530

    skills added

--- app/components/Features.tsx
+++ app/components/Features.tsx
@@ -1,7 +1,22 @@
+import { features } from "@/public/data";
+
 export const Features = () => {
 	return (
 		<div className="h-full w-full bg-white  text-black flex items-center justify-center">
-			FEATURES
+			<div className="flex flex-col gap-10 p-10">
+				<h1 className="font-righteous text-6xl">FEATURES</h1>
+				<div className="grid grid-cols-3 gap-20 w-full">
+					{features.map((feature, i) => (
+						<div
+							key={i}
+							className="flex flex-col justify-between text-center items-center gap-2 font-orbitron"
+						>
+							<h1 className="text-3xl ">{feature.title}</h1>
+							<p className="text-sm">{feature.desc}</p>
+						</div>
+					))}
+				</div>
+			</div>
 		</div>
 	);
 };
