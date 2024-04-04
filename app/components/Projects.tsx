a33a848a8a2faf97eb3fc07b540979d59eac1fc9
HrishikShaji <hrishikminishaji@gmail.com>
2024-04-04 16:50:44 +0530

    projects added

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -1,7 +1,21 @@
+import { projects } from "@/public/data";
+
 export const Projects = () => {
 	return (
-		<div className="h-full w-full bg-black  text-white flex items-center justify-center">
-			Projects
+		<div className="h-full w-full bg-black  text-white flex flex-col  ">
+			<div className="flex flex-col h-full gap-0 w-full">
+				<div className="h-full border-white border-b-2">
+					<h1 className="text-[15vh] font-righteous  ">PROJECTS</h1>
+				</div>
+				{projects.map((item, i) => (
+					<div
+						className="font-righteous bg-black border-white border-b-2 text-white text-xl h-full"
+						key={i}
+					>
+						{item.title}
+					</div>
+				))}
+			</div>
 		</div>
 	);
 };
