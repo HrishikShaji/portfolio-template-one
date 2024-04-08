0529c076eec7caaec759af6c3729b2ffef3158d7
HrishikShaji <hrishikminishaji@gmail.com>
2024-04-08 21:36:00 +0530

    modified features section

--- app/components/Features.tsx
+++ app/components/Features.tsx
@@ -2,19 +2,21 @@ import { features } from "@/public/data";
 import { Container } from "./Container";
 
 export const Features = () => {
-	return (
-		<Container textColor="black" bgColor="white" title="FEATURES">
-			<div className="grid grid-cols-3 gap-20 w-full">
-				{features.map((feature, i) => (
-					<div
-						key={i}
-						className="flex flex-col justify-between text-center items-center gap-2 font-orbitron"
-					>
-						<h1 className="text-3xl ">{feature.title}</h1>
-						<p className="text-sm">{feature.desc}</p>
-					</div>
-				))}
-			</div>
-		</Container>
-	);
+  return (
+    <Container textColor="black" bgColor="white" title="FEATURES">
+      <div className="flex h-full items-center justify-center">
+        <div className="grid grid-cols-3 gap-20 w-full  ">
+          {features.map((feature, i) => (
+            <div
+              key={i}
+              className="flex flex-col justify-between text-center items-center gap-2 font-orbitron"
+            >
+              <h1 className="text-3xl ">{feature.title}</h1>
+              <p className="text-sm">{feature.desc}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </Container>
+  );
 };
