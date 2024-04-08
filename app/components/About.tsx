c3d00f494e24a9ce0e2b5b1f76fad031f78d1ef7
HrishikShaji <hrishikminishaji@gmail.com>
2024-04-08 21:32:14 +0530

    about modified

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -1,10 +1,20 @@
-import { about } from "@/public/data";
+import { about, personal } from "@/public/data";
 import { Container } from "./Container";
+import Image from "next/image";
 
 export const About = () => {
-	return (
-		<Container textColor="white" bgColor="black" title="ABOUT">
-			<p className="font-orbitron text-xl">{about}</p>
-		</Container>
-	);
+  return (
+    <Container textColor="white" bgColor="black" title="ABOUT">
+      <div className="flex gap-10 h-full items-center">
+        <Image
+          src={personal.profileImg}
+          height={1000}
+          width={1000}
+          alt="image"
+          className="h-full w-[500px] object-cover"
+        />
+        <p className="font-orbitron text-xl">{about}</p>
+      </div>
+    </Container>
+  );
 };
