4c69cdd9a1f7bfd08d4d01c214c17129a5516abb
HrishikShaji <hrishikminishaji@gmail.com>
2024-04-09 21:45:15 +0530

    modified projects

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -4,14 +4,16 @@ import { Container } from "./Container";
 export const Projects = () => {
 	return (
 		<Container bgColor="white" textColor="black" title="PROJECTS">
-			{projects.map((item, i) => (
-				<div
-					className="font-righteous bg-black border-white border-b-2 text-white text-xl h-full"
-					key={i}
-				>
-					{item.title}
-				</div>
-			))}
+			<div className="flex flex-col h-full">
+				{projects.map((item, i) => (
+					<div
+						className="font-righteous  border-black border-b-2 flex items-center text-black text-xl h-full"
+						key={i}
+					>
+						<h1 className="text-3xl">{item.title}</h1>
+					</div>
+				))}
+			</div>
 		</Container>
 	);
 };
