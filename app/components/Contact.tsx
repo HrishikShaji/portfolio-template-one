d1b1bc40225bfe7e66cc224fa8ad0f0839d3a329
HrishikShaji <hrishikminishaji@gmail.com>
2024-04-10 22:39:14 +0530

    added contact

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -1,9 +1,25 @@
 import { Container } from "./Container";
 
 export const Contact = () => {
-	return (
-		<Container bgColor="black" textColor="white" title="CONTACT">
-			<div></div>
-		</Container>
-	);
+  return (
+    <Container bgColor="black" textColor="white" title="CONTACT">
+      <form className="flex flex-col gap-5">
+        <input
+          className="p-2 border-b-2 border-white bg-transparent font-orbitron text-4xl"
+          placeholder="Name..."
+        />
+        <input
+          className="p-2 border-b-2 border-white bg-transparent font-orbitron text-4xl"
+          placeholder="Email..."
+        />
+        <textarea
+          className="p-2 border-b-2 border-white bg-transparent font-orbitron text-4xl"
+          placeholder="Message..."
+        />
+        <button className="px-10 w-fit py-5  rounded-r-full font-orbitron bg-white text-sm text-black">
+          SEND MESSAGE
+        </button>
+      </form>
+    </Container>
+  );
 };
